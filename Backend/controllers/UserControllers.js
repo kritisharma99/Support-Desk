c425ae0c9b0f8dbb848e263bf02d035c883edcd7
kritisharma99 <sharmakriti.k27@gmail.com>
2023-01-10 15:50:00 +0530

    Registeration

--- Backend/controllers/UserControllers.js
+++ Backend/controllers/UserControllers.js
@@ -16,6 +16,34 @@ const registerUser = async(req,res)=>{
     }
 
     //User already existss
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+        return res.status(400).json({
+            message: "User already present"
+        })
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password,salt)
+
+    //create user
+    const user = User.create({
+        name,
+        email,
+        password:hashedPassword
+    })
+    //if user were created 
+    if(user){
+        res.status(200).json({
+            _id:(await user).id,
+            name: (await user).name,
+            email: (await user).email,
+            //token
+            message: "User successfully created"
+        })
+    }
 }
  /*********************************************
  * @loginUser a function handles sign up activity
